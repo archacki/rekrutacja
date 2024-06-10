ac217f76271390437fb9a64bfa6e0d1568f43b9f
archacki <jakub.archacki@gmail.com>
2024-06-10 12:34:49 +0200

    v3

--- main.py
+++ main.py
@@ -3,20 +3,16 @@ import sys
 from Processors.CSVProcessor import CSVProcessor
 from Processors.JSONProcessor import JSONProcessor
 
-# Dodanie folderu Processors do ścieżki modułów Pythona
 processors_path = os.path.join(os.path.dirname(__file__), 'Processors')
 sys.path.append(processors_path)
 
-# Pobieranie danych od użytkownika
 column = int(input('Column: '))
 row = int(input('Row: '))
 object_index = int(input('ObjectID: '))
 
-# Ścieżki do plików
 csv_file_path = os.path.join('data', 'Rekrucsv.csv')
 json_file_path = os.path.join('data', 'rekrujs.json')
 
-# Uruchamianie klasy CSVProcessor
 csv_processor = CSVProcessor(csv_file_path)
 lines = csv_processor.count_lines()
 value = csv_processor.get_value(column, row)
@@ -29,7 +25,6 @@ print(f'Value in column {column} row {row}: {value}')
 print(f'Value type: {value_type}')
 print('##############')
 
-# Uruchamianie klasy JSONProcessor
 json_processor = JSONProcessor(json_file_path)
 json_object_count = json_processor.count_objects()
 json_absolute_path = json_processor.get_absolute_path()
